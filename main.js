fb4bc8e855d0efe8e8ca7180d143200c305a5c03
OhMedrano <Omedrano564@gmail.com>
2014-01-12 04:38:12 -0500

    New changes

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
-	var sel = true
+	$("#gselect").click(function(){
+		$("#guitarsel").toggle("slow");
 
-	$("#gselect").click(function()
-	{	
-		if (sel==true) {
-		$("#guitarsel").show("slow")
-		sel = false
-
-	}
-		else {
-			$("guitarsel").hide("slow")
-			 sel = true
-		}
 	});
+
+	$(".slide").click(function(){
+		$("#menu").slideToggle("fast");
+	});
+
+	$("td img").hover(function(){
+		$(".select").slideToggle('fast');
+	})
+
 });
+	
+
+	
